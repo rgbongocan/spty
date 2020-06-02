5252f41ec95925adb65545dc2681e4fdc42f137a
Renzo Gabriel Bongocan <renzo@kalibrr.com>
2020-06-02 15:57:47 +0800

    Add more info to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
 setup(
-    name="spfy",
+    name="spty",
     version="0.1",
     py_modules=["cli"],
     install_requires=["spotipy", "click", "click-aliases"],
     entry_points="""
         [console_scripts]
-        spfy=cli:cli
+        spty=cli:cli
     """,
+    author="Renzo Gabriel Bongocan",
+    author_email="gabrielbongocan@gmail.com",
+    description="A simple spotify cli",
+    url="https://github.com/rgbongocan/spty",
 )
