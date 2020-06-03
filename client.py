6f9b99156eb2dec3404c95169c613267483aba54
Renzo Gabriel Bongocan <renzo@kalibrr.com>
2020-06-03 14:34:02 +0800

    Better prompt with defaults

--- client.py
+++ client.py
@@ -18,17 +18,22 @@ yaml = ruamel.yaml.YAML()
 
 @click.command()
 def configure():
-    click.echo(
-        "If you haven't yet, create your spotify app at https://developer.spotify.com/dashboard/"
-    )
-    config = {
-        "username": click.prompt("Spotify username", type=str),
-        "client_id": click.prompt("Client ID", type=str),
-        "client_secret": click.prompt("Client Secret", type=str),
-        "redirect_uri": click.prompt("Redirect URI", type=str),
+    click.echo("Acquire your config from https://developer.spotify.com/dashboard/")
+    with open("config.yaml", "r") as fp:
+        config = yaml.load(fp) or {}
+    # placing it outside of write block so the og config
+    # is preserved in case the user aborts the prompt
+    new_config = {
+        key: click.prompt(text, default=config.get(key), type=str)
+        for key, text in [
+            ("username", "Spotify username"),
+            ("client_id", "Client ID"),
+            ("client_secret", "Client Secret"),
+            ("redirect_uri", "Redirect URI"),
+        ]
     }
     with open("config.yaml", "w") as fp:
-        yaml.dump(config, fp)
+        yaml.dump(new_config, fp)
 
 
 def get_spotify_client():
