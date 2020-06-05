dcd364c3fbded9de7c9aaaf8c5f2e7eec0854339
Renzo Gabriel Bongocan <renzo@kalibrr.com>
2020-06-05 23:08:31 +0800

    Make spty downloadable/installable

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import click
+import hashlib
 import ruamel.yaml
 import spotipy
 
+from pathlib import Path
 from spotipy import util
 
 config = {}
+CONFIG_FILE_PATH = f"{Path.home()}/spty.yaml"
 SCOPES = [
     "user-read-playback-state",
     "user-read-currently-playing",
@@ -18,8 +21,9 @@ yaml = ruamel.yaml.YAML()
 
 def prompt_config():
     click.echo("Acquire your config from https://developer.spotify.com/dashboard/")
-    with open("config.yaml", "r") as fp:
-        config = yaml.load(fp) or {}
+    with open(CONFIG_FILE_PATH, "a+") as fp:
+        fp.seek(0)
+        config = yaml.load(fp) or {"redirect_uri": "http://localhost:9090/"}
     # placing outside of write block so the og config
     # is preserved in case the user aborts the prompt
     new_config = {
@@ -31,7 +35,7 @@ def prompt_config():
             ("redirect_uri", "Redirect URI"),
         ]
     }
-    with open("config.yaml", "w") as fp:
+    with open(CONFIG_FILE_PATH, "w+") as fp:
         yaml.dump(new_config, fp)
     return new_config
 
@@ -44,8 +48,9 @@ def configure_command():
 
 def configure():
     global config
-    with open("config.yaml", "r") as fp:
-        _config = yaml.load(fp)
+    with open(CONFIG_FILE_PATH, "a+") as fp:
+        fp.seek(0)
+        _config = yaml.load(fp) or {}
         keys = {"username", "client_id", "client_secret", "redirect_uri"}
         misconfigured = not all(_config.get(k) for k in keys)
         if misconfigured:
@@ -61,12 +66,15 @@ def get_spotify_client():
         configure()
     if not sp:
         username = config["username"]
+        scopes = " ".join(SCOPES)
+        scope_hash = hashlib.sha1(scopes.encode("utf-8")).hexdigest()
         token = util.prompt_for_user_token(
             username,
             scope=" ".join(SCOPES),
             client_id=config["client_id"],
             client_secret=config["client_secret"],
             redirect_uri=config["redirect_uri"],
+            cache_path=f"{Path.home()}/.cache-{username}-{scope_hash[:16]}",
         )
         if token:
             sp = spotipy.Spotify(auth=token)
